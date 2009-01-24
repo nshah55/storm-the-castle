67e7de1d465d64e45a87e2c12e587d0c8a4993ec
chornsokun@gmail.com <chornsokun@gmail.com@24b2f260-e9d0-11dd-b2d9-215335d0b316>
2009-01-24 04:45:16 +0000

    initial check in try F5 ;)

---
+++ trunk/GettingStartMonoRailAndActiveRecord/MRSample/Controllers/HowToController.cs
@@ -0,0 +1,35 @@
+﻿namespace MRSample.Controllers
+{
+	using Castle.MonoRail.Framework;
+	using Models;
+
+	[Layout("syntaxHightlight")]
+	public class HowToController: SmartDispatcherController
+	{
+
+		public void Index()
+		{
+			
+		}
+
+		public void UsingController()
+		{
+			
+		}
+
+		public void UsingBrail()
+		{
+			
+		}
+
+		public void UsingActiveRecord()
+		{
+			
+		}
+
+		public void Integrate()
+		{
+			
+		}
+	}
+}
---
+++ trunk/GettingStartMonoRailAndActiveRecord/MRSample/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MRSample
+{
+	using System.Reflection;
+	using Castle.ActiveRecord;
+	using Castle.ActiveRecord.Framework;
+	using Castle.ActiveRecord.Framework.Config;
+
+	public class Global : System.Web.HttpApplication
+	{
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			IConfigurationSource config = ActiveRecordSectionHandler.Instance;
+
+			Assembly assm = Assembly.Load("MRSample");
+
+			ActiveRecordStarter.Initialize(assm, config);
+		}
+	}
+}
