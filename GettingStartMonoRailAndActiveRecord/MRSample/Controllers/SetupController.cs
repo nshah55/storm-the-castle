122934e261c262f4a623fe0231b3592051fe79db
chornsokun <chornsokun@gmail.com>
2009-01-24 04:45:16 +0000

    initial check in try F5 ;)

---
+++ GettingStartMonoRailAndActiveRecord/MRSample/Controllers/SetupController.cs
@@ -0,0 +1,58 @@
+﻿namespace MRSample.Controllers
+{
+	using System;
+	using System.IO;
+	using Castle.ActiveRecord;
+	using Castle.MonoRail.Framework;
+
+	[Rescue("generalerror")]
+	public class SetupController: SmartDispatcherController
+	{
+
+		public void Index()
+		{
+		}
+
+		public void Install(bool confirm)
+		{
+			if (Request.IsLocal && confirm)
+			{
+				try
+				{
+					ActiveRecordStarter.CreateSchema();
+					Flash["msg"] = "Schema generated.";
+				}
+				catch (Exception ex)
+				{
+					throw new Exception("Schema geneneration fail !", ex);
+				}
+			}
+			else
+			{
+				return;
+			}
+		}
+
+		public void Uninstall(bool confirm)
+		{
+			Flash["msg"] = "";
+			if (Request.IsLocal && confirm)
+			{
+				try
+				{
+					ActiveRecordStarter.DropSchema();
+
+					Flash["msg"] = "Schema dropped.";
+				}
+				catch (Exception ex)
+				{
+					throw new Exception("Drop fail !", ex);
+				}
+			}
+			else
+			{
+				return;
+			}
+		}
+	}
+}
---
+++ GettingStartMonoRailAndActiveRecord/MRSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MRSample.Global" Language="C#" %>
